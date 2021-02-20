cc6bb723e9e307eb4d4eca9021f1bbd5075c7b90
daishuyang001 <daishuyang001@ke.com>
2021-02-20 15:25:33 +0800

    init project]

---
+++ src/main/java/com/find/any/FindAnyToolApplication.java
@@ -0,0 +1,13 @@
+package com.find.any;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAnyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAnyToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/find/any/FindAnyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.find.any;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAnyToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
